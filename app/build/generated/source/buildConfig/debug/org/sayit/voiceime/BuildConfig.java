bf83c2acad0b8c669a56d5657e3a5cfb0e17c6ef
jing-gou <2506437479@qq.com>
2026-05-23 19:33:15 +0800

    feat: 完善权限配置，配置api接口

---
+++ app/build/generated/source/buildConfig/debug/org/sayit/voiceime/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.sayit.voiceime;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.sayit.voiceime";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String ASR_ACCESS_KEY_ID = "";
+  // Field from default config.
+  public static final String ASR_ACCESS_KEY_SECRET = "";
+  // Field from default config.
+  public static final String ASR_APP_ID = "";
+  // Field from default config.
+  public static final String ASR_WS_URL = "";
+}
