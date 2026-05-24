4bf9cb85f9d961dd76bdc1cb936f062d55ab9b33
Sugar <110728714+jing-gou@users.noreply.github.com>
2026-05-24 23:17:02 +0800

    Revert "Fixed/blankheight"

--- app/src/main/java/org/sayit/voiceime/clipboard/ClipboardEntry.kt
+++
@@ -1,9 +0,0 @@
-package org.sayit.voiceime.clipboard
-
-import java.util.UUID
-
-data class ClipboardEntry(
-    val id: String = UUID.randomUUID().toString(),
-    val text: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
